65e0f87e92412c0729ed1fc1f048cbd69da3b083
cheungkinwong <cheungkin.wong@gmail.com>
2019-06-20 12:00:54 +0200

    add readme

--- js/main.js
+++ js/main.js
@@ -8,23 +8,24 @@ let myChoice = "make a choice";
 
 iChoose.forEach(ready);
 function ready(rps) {
-  rps.addEventListener("click", function() {
-    myChoice = rps.innerHTML;
-    if (myChoice === "ROCK") {
-      imgMe.innerHTML =
-        '<i class="far fa-5x fa-hand-rock fa-flip-vertical"></i>';
-    } else if (myChoice === "PAPER") {
-      imgMe.innerHTML =
-        '<i class="far fa-5x fa-hand-paper fa-flip-vertical"></i>';
-    } else {
-      imgMe.innerHTML =
-        '<i class="far fa-5x fa-hand-scissors fa-rotate-270"></i>';
-    }
-    document.getElementById("rock").style.display = "none";
-    document.getElementById("paper").style.display = "none";
-    document.getElementById("scissor").style.display = "none";
-    return myChoice;
-  });
+  rps.addEventListener("click", choose);
+}
+
+function choose(rps) {
+  myChoice = rps.innerHTML;
+  if (myChoice === "ROCK") {
+    imgMe.innerHTML = '<i class="far fa-7x fa-hand-rock fa-flip-vertical"></i>';
+  } else if (myChoice === "PAPER") {
+    imgMe.innerHTML =
+      '<i class="far fa-7x fa-hand-paper fa-flip-vertical"></i>';
+  } else {
+    imgMe.innerHTML =
+      '<i class="far fa-7x fa-hand-scissors fa-rotate-270"></i>';
+  }
+  document.getElementById("rock").disabled = true;
+  document.getElementById("paper").disabled = true;
+  document.getElementById("scissor").disabled = true;
+  return myChoice;
 }
 
 pcChoose.addEventListener("click", set);
@@ -34,17 +35,17 @@ function set() {
   } else {
     let random = Math.floor(Math.random() * 3);
     if (random === 0) {
-      imgPc.innerHTML = '<i class="far fa-5x fa-hand-rock"></i>';
+      imgPc.innerHTML = '<i class="far fa-7x fa-hand-rock"></i>';
       pcChoice = "ROCK";
     } else if (random === 1) {
-      imgPc.innerHTML = '<i class="far fa-5x fa-hand-paper"></i>';
+      imgPc.innerHTML = '<i class="far fa-7x fa-hand-paper"></i>';
       pcChoice = "PAPER";
     } else {
       imgPc.innerHTML =
-        '<i class="far fa-5x fa-hand-scissors fa-rotate-90"></i>';
+        '<i class="far fa-7x fa-hand-scissors fa-rotate-90"></i>';
       pcChoice = "SCISSOR";
     }
-    document.getElementById("pc").style.display = "none";
+    document.getElementById("pc").disabled = "true";
 
     go(pcChoice);
   }
@@ -81,11 +82,12 @@ reset.addEventListener("click", empty);
 function empty() {
   pcChoice = "";
   myChoice = "make a choice";
-  imgMe.innerHTML = '<i class="far fa-5x fa-question-circle"></i>';
-  imgPc.innerHTML = '<i class="far fa-5x fa-question-circle"></i>';
+  imgMe.innerHTML = '<i class="far fa-7x fa-question-circle"></i>';
+  imgPc.innerHTML = '<i class="far fa-7x fa-question-circle"></i>';
   result.innerHTML = "Please make a choice";
-  document.getElementById("rock").style.display = "inline";
-  document.getElementById("paper").style.display = "inline";
-  document.getElementById("scissor").style.display = "inline";
-  document.getElementById("pc").style.display = "inline";
+  document.getElementById("rock").disabled = false;
+  document.getElementById("paper").disabled = false;
+  document.getElementById("scissor").disabled = false;
+  document.getElementById("pc").disabled = false;
+  console.log("lol");
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+Excercise Schaar steen Papier
+
+Learning objectives (if this is a Learning Challenge)
+generate random
+if statements
+
+By Cheung Kin Wong
+
+https://cheungkinwong.github.io/Schaar-Steen-Papier/
